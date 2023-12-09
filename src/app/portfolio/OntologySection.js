be1ce090ef69c2c6d2a3b0f1ade99bd341cc153a
Noah Putnam <noahputnam@gmail.com>
2023-12-09 22:02:57 +0000

    32rtangible1

--- src/app/portfolio/OntologySection.js
+++ src/app/portfolio/OntologySection.js
@@ -111,7 +111,7 @@ const OntologySection = () => {
         <div className={styles.row__smalltext}>
           <p>
             A basic example of an app built on top of a Vibe is an image studio
-            that given a selected Vibe applies techniques like{" "}
+            that, given a selected Vibe, applies techniques like{" "}
             <Link href="https://huggingface.co/blog/lora" target="_blank">
               LoRA
             </Link>{" "}
