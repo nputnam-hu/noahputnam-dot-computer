a4a13caaa6d848cf95a275644e2638a2a7cfd402
Noah Putnam <noahputnam@gmail.com>
2026-01-14 15:32:16 -0500

    Change link href from '/work' to '/portfolio'

--- src/app/page.js
+++ src/app/page.js
@@ -71,7 +71,7 @@ export default function Home() {
             About
           </Link>
           <Link
-            href={{ pathname: "/work", params: { from: "home" } }}
+            href={{ pathname: "/portfolio", params: { from: "home" } }}
             style={{
               fontFamily: "Times New Roman",
               fontSize: 17,
