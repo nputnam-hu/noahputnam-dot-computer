3d71490ed89b97c460c146e80d93e5145897bb8c
Noah Putnam <noahputnam@gmail.com>
2026-08-10 02:42:58 -0400

    Swap Twitter and Are.na links in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -71,18 +71,6 @@ export default function Home() {
             About
           </Link>
           <Link
-            href={{ pathname: "https://twitter.com/noah_putnam", params: { from: "home" } }}
-            target="_blank"
-            style={{
-              fontFamily: "Times New Roman",
-              fontSize: 17,
-              marginTop: 6,
-              color: "blue",
-            }}
-          >
-            Twitter
-          </Link>
-          <Link
             href="https://www.are.na/noah-putnam/love-always-wins"
             target="_blank"
             style={{
@@ -93,6 +81,18 @@ export default function Home() {
             }}
           >
             Are.na
+          </Link>
+          <Link
+            href={{ pathname: "https://twitter.com/noah_putnam", params: { from: "home" } }}
+            target="_blank"
+            style={{
+              fontFamily: "Times New Roman",
+              fontSize: 17,
+              marginTop: 6,
+              color: "blue",
+            }}
+          >
+            Twitter
           </Link>
           <Link
             href={{ pathname: "/work", params: { from: "home" } }}
